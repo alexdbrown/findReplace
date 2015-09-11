72d3e40b140e611cea6975b1a377c2ac1af2c6f6
alexandra <alexdbrown@gmail.com>
2015-09-11 12:29:32 -0700

    write in notes for review

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,13 @@ var findReplace = function(inputString, matchedWord, replacedWord) {
     var newWord = "";
     var newString = "";
 
+
     for (var i = 0; i < splitInputString.length; i++) {
         if (splitInputString[i] === matchedWord) {
             newWord = splitInputString[i];
             newString = inputString.replace(newWord, replacedWord);
             return newString;
-        }
+        } 
     }
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,8 @@
 describe('findReplace', function() {
+    //*note for code review - let's talk about writing out specs that will always
+    //*work, I seem to do this a lot where when I get to the meat of it my functionality
+    // *ends up not working anymore
+
     // it('finds a word in a string of words', function() {
     //     expect(findReplace('down to clown', 'clown', 'party')).to.equal('clown');
     // });
